db52cabfb164ccefafa9ef6e7b7f012ea9ced921
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-07-15 11:20:37 -0500

    create wtform EditProfileForm

--- forms.py
+++ forms.py
@@ -23,3 +23,15 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+class EditProfileForm(FlaskForm):
+    """Edit user profile information form."""
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Optional) Image URL')
+    bio = StringField('(Optional) Bio information')
+    location = StringField('(Optional) User Location')
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+
